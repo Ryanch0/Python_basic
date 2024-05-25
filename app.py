55782009fbc16d9136366e069361a482d1efb891
Ryan <tjddnfkdls0@naver.com>
2024-05-25 17:51:09 +0900

    first commit

---
+++ app.py
@@ -0,0 +1,25 @@
+중고차재고 = ['mercedes' , 'tico','bmw']
+if 'tico2' in 중고차재고 : 
+    print('Yup')
+else :
+        print('dd')
+
+재고량 = 90
+if  재고량 > 0 : 
+    print('지금 주문 가능합니다')
+else : 
+    print('nono')
+
+for i in 중고차재고 :
+    print( i +' is available')
+
+def hat(a):
+    print(a + 1)
+
+hat(3)
+
+def hamsu() : 
+    hat(4)
+    return 'gggggg'
+
+print(hamsu())
