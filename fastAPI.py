4b92f749f64f3ed3001738894983458659e9a99a
Ryan <tjddnfkdls0@naver.com>
2024-06-29 18:07:21 +0900

    query validation

--- fastAPI.py
+++ fastAPI.py
@@ -1,4 +1,4 @@
-from fastapi import FastAPI
+from fastapi import FastAPI,Query
 from typing import Optional
 from enum import Enum
 from pydantic import BaseModel
@@ -11,6 +11,7 @@ app = FastAPI()
 def read_root():
     return {"Hello" : "World"}
 
+#Enum
 class FoodEnum(str, Enum):
     fruits = "fruits"
     vegetables = "vegetables"
@@ -26,8 +27,9 @@ async def get_food(food_name : FoodEnum):
     
     return {"food_name": food_name, "message" :  "you must have chosen dairy right?"}
 
+#url parameter, q(query string), short(boolean)
 @app.get("/items/{item_id}")
-async def read_item(item_id : int, q : Optional[str] = None, short : bool = False):
+async def read_item(item_id : int, q : Optional[str] = Query(None, min_length=3 ,max_length=10), short : bool = False):
     item = {"item_id":item_id}
     if q:
        item.update({"q":q})
@@ -48,6 +50,7 @@ async def get_user_item(user_id : int, item_id : int, q:Optional[str] = None, sh
         })
     return item
 
+#Response Body BaseModel
 class Item(BaseModel):
     name : str
     description : Optional[str] = None
@@ -63,8 +66,15 @@ async def create_item(item: Item):
     return item_dict
 
 @app.put("/items/{item_id}")
-async def create_item_with_put(item_id : int, item:Item, q:Optional[str] = None):
+async def create_item_with_put(item_id : int, item:Item, q:Optional[str] = Query(None, max_length = 10)):
     result = {"item_id" : item_id, **item.dict()}
     if q:
         result.update({"q" : q})
     return result
+
+#hidden query
+@app.get("/items_hidden")
+async def hidden_query_route(hidden_query: Optional[str]= Query(None, include_in_schema = False)):
+    if hidden_query:
+        return {"hidden_query" : hidden_query}
+    return {"hidden_query" : "Not found"}
